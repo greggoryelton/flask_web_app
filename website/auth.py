d832751fb388ab61ee5c44840cd7fd90a7e593ed
Greggory Elton <greggory.elton@outlook.com>
2022-08-05 10:31:47 -0400

    Some REST handling

--- website/auth.py
+++ website/auth.py
@@ -1,13 +1,26 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request
 
-auth = Blueprint('auth',__name__)
-
-@auth.route('/login')
-def dev_login():
-    return "<p>Login</p>"
-
-@auth.route('/logout')
-def dev_logout():
-    return "<p>Logout</p>"
+auth = Blueprint('auth', __name__)
 
 
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template('login.html')
+
+
+@auth.route('/logout', methods=['POST'])
+def logout():
+    return '<p>Logout</p>'
+
+
+@auth.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        user = request.form.get('user')
+        firstname = request.form.get('first')
+        lastname = request.form.get('last')
+        email = request.form.get('email')
+        password = request.form.get('pass')
+        print(user, firstname, lastname, email, password)
+
+    return render_template('signup.html')
--- website/views.py
+++ website/views.py
@@ -1,8 +1,12 @@
+import json
+
 from flask import Blueprint, render_template
 
-views = Blueprint('views',__name__)
+views = Blueprint('views', __name__)
+
 
 @views.route('/')
 def homepage():
     return render_template('index.html')
+
 
